4a1904bd5e397c48f41ca31791c70fb8f278064b
gabriellahartanto <gabriella.hartanto@icloud.com>
2020-11-06 14:25:00 +0700

    initialized

---
+++ ice-cream/pages/index.js
@@ -0,0 +1,42 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>brie</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Gabriella Hartanto</h1>
+
+        {/* <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p> */}
+
+        <div className={styles.grid}>
+          {/* <a href="https://nextjs.org/docs" className={styles.card}>
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a> */}
+
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>&copy; <b>greentea</b> twenty-twenty</p>
+        {/* <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a> */}
+      </footer>
+    </div>
+  )
+}
